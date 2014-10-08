c3d70519bc6484c228e74c018daedeb10c6b03c7
Mischa The Evil <mischa_the_evil@hotmail.com>
2014-10-08 07:19:45 +0200

    Import demonstration plugin for Redmine message #44230.

---
+++ app/controllers/searcher_controller.rb
@@ -0,0 +1,13 @@
+class SearcherController < ApplicationController
+  unloadable
+
+
+  def index
+    # incorrect syntaxis used
+    #@abc = Setting.plugin_searcher['abc']      
+    @abc = Setting['plugin_searcher'][:abc]
+
+    #not needed, Rails handles this in this case
+    #render :action => 'index'
+  end
+end
---
+++ init.rb
@@ -0,0 +1,17 @@
+#not needed anymore on Redmine 2.x
+#require 'redmine'
+Redmine::Plugin.register :searcher do
+  name 'Searcher plugin'
+  author 'Erick Andrade (modified for testing purposes by Mischa The Evil)'
+  description 'A plugin for demonstrating Redmine message #44230'
+  version '0.0.1'
+  url 'https://github.com/MischaTheEvil/searcher'
+  author_url 'https://github.com/MischaTheEvil'
+
+  settings :default => {
+    :abc  => 'xxx',
+    :abc2 => 'yyy'
+  }, :partial => 'settings/searcher_settings'
+
+  menu :top_menu, :searcher, { :controller => 'searcher', :action => 'index' }, :caption => 'Searcher'
+end
